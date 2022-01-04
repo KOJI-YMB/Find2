86527c0d6a855673a00a9b350a5597e57d8b4f3b
Koji Yamabe <kojiy@donuts-bkk.com>
2022-01-04 08:20:38 +0700

    add routes

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,18 +1,18 @@
 class Public::OrdersController < ApplicationController
 
-  def new
-  end
-  
-  def complete
-  end
-
-  def create
-  end
-
   def index
   end
 
   def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+  
+  def complete
   end
 
   def confirm
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'orders/create'
-    get 'orders/show'
-    get 'orders/confirm'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-  end
+  
   devise_for :users, controllers: {
     session: 'users/sessions',
     registrations: 'usrs/redistrations',
@@ -17,5 +8,16 @@ Rails.application.routes.draw do
   
   namespace :public do
     root 'homes#top'
+
+    resources :orders, only: [:index, :show, :create, :new]
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/complete' => 'orders#complete'
+    resources :users, only: [:show, :edit, :update]
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :show, :create, :destroy, :update] do
+      collection do
+        delete 'all_destroy'
+      end
+    end
   end
 end
